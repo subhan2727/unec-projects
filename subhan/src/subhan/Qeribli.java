f2325aa4d56406adc82cdb04baaf81ddaa898943
subhan2727 <105320322+subhan2727@users.noreply.github.com>
2022-05-11 02:22:18 +0400

    seher

---
+++ subhan/src/subhan/Qeribli.java
@@ -0,0 +1,66 @@
+package subhan;
+
+public class Qeribli {
+
+	public static void main(String[] args) {
+//byte 128-e kimi tam ededler
+//int tam ədədlər ;doouble tam və kəsr ədədlər
+//char sadəcə bir hərf 'c';  String hərif,söz,cümlə	"haqisda"
+//boolean sadəcə "true;false" ve ya direk cavab
+//float yaddaşda daha az yer tutsun deyə istifadə olunur 
+//long int kimidir sadace intden daha boyukdur
+	
+		
+		
+		
+    byte o=127;
+		
+	System.out.println("hello world");
+    int x=5;
+    int y=10;
+     int z=x+y;
+	System.out.println(z);
+	
+	int c=x-y;
+	System.out.println(c);
+	int v=x*y;
+	System.out.println(v);
+	
+	double a=10;
+	double b=5;
+	double bolme= b/a;
+	System.out.println(bolme);
+	 
+	char s='S';
+	System.out.println(s);
+	String soz= "alma agacda qaldi";
+	System.out.println(" soz= " +soz);
+	
+	boolean cavab=true;
+	System.out.println(cavab);
+	boolean cav=false;
+	System.out.println(cav);
+   
+	int h=5;
+	System.out.println("h="+h);
+	double d=5.5;
+	
+	System.out.println("d="+d);
+    boolean n=true;//5>4	
+    System.out.println("n="+n);	
+	
+    float f=(float) d;
+	float f2=5.5f;// meslehet gorulmur
+	System.out.println("f="+f);
+	System.out.println("f2="+f2);
+	
+    long l= 567878788;
+    System.out.println(l);
+    
+	
+	
+	
+	
+	}
+
+}
