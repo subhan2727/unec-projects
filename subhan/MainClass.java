2472d5958c72985a689191e7e5fe1f744dab8107
subhan2727 <105320322+subhan2727@users.noreply.github.com>
2022-05-17 02:27:56 +0400

    unec-projects

---
+++ subhan/MainClass.java
@@ -0,0 +1,40 @@
+
+package subhan;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+ 
+
+		int u=0;
+		int r=0;
+		for (int k=1;k<=1000;k++) {
+		u++;
+		r+=u;}
+		System.out.print(r);	
+	
+	int saygac=0;
+	for(int h=1;h<=1000;h++);{
+	int h=1;
+	saygac+=h;}
+	System.out.println(saygac);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}	
+	}
+    
+	
+	
+	
+	
+
---
+++ subhan/src/subhan/IlkKodlarim.java
@@ -0,0 +1,47 @@
+package subhan;
+
+public class IlkKodlarim {
+
+	public static void main(String[] args) {
+	
+	int age=17;
+	if(age>=18) {
+     System.out.println("olar");}
+	else {
+	System.out.println("olmaz");
+	}
+	
+	int sex=0;
+	if(sex<=1) {
+	System.out.println("kisi");
+	}
+	else {
+		System.out.println("qadin");}
+   
+	
+	int gender=1;
+	if(gender<=2) {
+	System.out.println("man");
+	}
+	else {
+		System.out.println("woman");}
+	
+	
+	
+	
+	
+	
+	
+	}
+	
+	
+	
+	 
+	
+	
+   
+
+  
+		
+	
+}
